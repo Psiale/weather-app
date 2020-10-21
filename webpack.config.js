f410fb505e7dc091689ff5f24bc54b66fd920f73
Alexis <psialedev@gmail.com>
2020-10-21 13:02:21 -0500

    trying stablishing path to /

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: '/',
   },
   plugins: [
     new Dotenv(),
