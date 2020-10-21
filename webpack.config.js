2392728d378e9eec37da02eb60cbfea65f9da82b
Alexis <psialedev@gmail.com>
2020-10-21 15:51:13 -0500

    changed the name of the dir to be 'build'

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     // path.resolve(__dirname, 'dist')
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   plugins: [
     new Dotenv(),
